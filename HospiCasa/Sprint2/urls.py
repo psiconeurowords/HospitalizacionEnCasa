2cfcb5dbdc04800914ad54dc4c0d4e827df50314
jorge pachon <jpachon@unbosque.edu.co>
2022-09-07 23:11:39 -0500

    archivos en limpio para crear clases, rutas y metodos

--- HospiCasa/Sprint2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('new', views.new, name='newMember'),
-    path('read', views.read, name='getMembers')
-]
